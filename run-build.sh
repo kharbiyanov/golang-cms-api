406c9971684722827d56e3d4585147a9fd0d4a06
Marat Kharbiyanov <kharbiyanov@gmail.com>
2020-03-17 17:34:08 +0500

    [fix] circleci

--- run-build.sh
+++ run-build.sh
@@ -2,4 +2,4 @@
 
 go build -buildmode=plugin -o plugins/posts.so gq/posts/*
 go build -buildmode=plugin -o plugins/translate.so gq/translate/*
-GOOS=linux go build -a -installsuffix cgo -o main .
+CGO_ENABLED=1 GOOS=linux go build -a -installsuffix cgo -o main .
