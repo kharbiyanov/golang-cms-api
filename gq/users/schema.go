d43ddec88c56766b36ae030d737cf8cbc808266b
Marat Kharbiyanov <kharbiyanov@gmail.com>
2020-05-07 19:09:28 +0500

    [new] userList

--- gq/users/schema.go
+++ gq/users/schema.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"cms-api/config"
 	"cms-api/models"
 	"cms-api/utils"
 	"github.com/graphql-go/graphql"
@@ -31,6 +32,35 @@ func setupQuery() {
 				return nil, err
 			}
 			return GetUser(params)
+		},
+	}
+	queryFields["userList"] = &graphql.Field{
+		Type: graphql.NewObject(graphql.ObjectConfig{
+			Name: "UserList",
+			Fields: graphql.Fields{
+				"data": &graphql.Field{
+					Type: graphql.NewList(UserType),
+				},
+				"count": &graphql.Field{
+					Type: graphql.Int,
+				},
+			},
+		}),
+		Description: "Get user list.",
+		Args: graphql.FieldConfigArgument{
+			"first": &graphql.ArgumentConfig{
+				Type:         graphql.Int,
+				DefaultValue: config.Get().DefaultPostsLimit,
+			},
+			"offset": &graphql.ArgumentConfig{
+				Type: graphql.Int,
+			},
+		},
+		Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+			if err := utils.ValidateUser(params, "user", "list"); err != nil {
+				return nil, err
+			}
+			return GetUserList(params)
 		},
 	}
 }
